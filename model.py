219fa852142652314a1741f245a4076d0b973024
SunwooYoo <swyoo23@postech.ac.kr>
2020-12-27 20:03:26 +0900

    fisrt commit

---
+++ model.py
@@ -0,0 +1,40 @@
+import torch.nn as nn
+import torch
+
+""" Optional conv block """
+def conv_block(in_channels, out_channels):
+
+    return nn.Sequential(
+        nn.Conv2d(in_channels, out_channels, 3, padding=1),
+        nn.BatchNorm2d(out_channels),
+        nn.ReLU(),
+    )
+
+
+""" Define your own model """
+class FewShotModel(nn.Module):
+    def __init__(self, x_dim=3):
+        super().__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(x_dim, 64, kernel_size=11, stride=4, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(64, 192, kernel_size=5, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(192, 384, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(384, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            
+        )
+
+
+    def forward(self, x):
+        x = self.features(x)
+        embedding_vector = x.view(x.size(0), -1)
+        return embedding_vector
+        
